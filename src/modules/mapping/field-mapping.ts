54eaf78d30326b027b3e1fb6315b88ae65986a49
In3luki <41452412+In3luki@users.noreply.github.com>
2023-10-26 21:18:43 +0200

    Speed up initial index translations by skipping nested values (#17)

--- src/modules/mapping/field-mapping.ts
+++ src/modules/mapping/field-mapping.ts
@@ -1,4 +1,10 @@
-import type { DynamicMapping, TranslatableData, TranslationEntry } from "@modules/babele/types.ts";
+import type {
+    Converter,
+    DynamicMapping,
+    TranslatableData,
+    TranslationEntry,
+    TranslationEntryData,
+} from "@modules/babele/types.ts";
 import { TranslatedCompendium } from "@modules";
 
 /**
@@ -9,7 +15,7 @@ import { TranslatedCompendium } from "@modules";
 class FieldMapping {
     field: string;
     path: string;
-    converter: Function | null;
+    converter: Converter | null;
     #dynamic: boolean;
     tc?: TranslatedCompendium;
 
@@ -18,7 +24,7 @@ class FieldMapping {
         this.tc = tc;
         if (typeof mapping === "object") {
             this.path = mapping["path"];
-            this.converter = game.babele.converters[mapping.converter] ?? null;
+            this.converter = (game.babele.converters[mapping.converter] as Converter) ?? null;
             this.#dynamic = true;
         } else {
             this.path = mapping;
@@ -35,20 +41,35 @@ class FieldMapping {
     /**
      * Translate the value and return expanded object
      */
-    map(data: TranslatableData, translations: TranslationEntry): Record<string, string> {
-        const value = this.translate(data, translations);
+    map(data: TranslatableData, translations: TranslationEntry, deep?: boolean): Record<string, unknown> {
+        const value = this.translate(data, translations, deep);
         if (value) {
             return expandObject<Record<string, string>>({ [this.path]: value });
         }
         return {};
     }
 
-    translate(data: Partial<TranslatableData>, translations: TranslationEntry): unknown {
+    translate(
+        data: Partial<TranslatableData>,
+        translations: TranslationEntry,
+        deep?: boolean
+    ): string | TranslatableData[] | TranslationEntryData | null {
         const originalValue = this.extractValue(data);
-        if (this.converter && originalValue) {
-            return this.converter(originalValue, translations[this.field], data, this.tc, translations);
+        // Is there something to translate?
+        if (originalValue) {
+            // Does a converter exist for this value?
+            if (this.converter) {
+                // Return early if the extracted value is an array and we don't want deep translations
+                if (!deep && Array.isArray(originalValue)) return null;
+                // Return the result of the converter function
+                return this.converter(originalValue, translations[this.field], data, this.tc, translations);
+            }
+            // Original value is a string and no converter. Return the extracted translation
+            if (typeof originalValue === "string") {
+                return translations[this.field];
+            }
         }
-        return translations[this.field];
+        return null;
     }
 
     /**
@@ -60,8 +81,8 @@ class FieldMapping {
      * console.log(value) // -> "bla bla"
      *
      */
-    extractValue(data: Partial<TranslatableData>): string {
-        return getProperty(data, this.path) as string;
+    extractValue(data: Partial<TranslatableData>): string | TranslatableData[] {
+        return getProperty(data, this.path) as string | TranslatableData[];
     }
 
     /**
@@ -72,7 +93,7 @@ class FieldMapping {
      * const value = new FieldMapping("desc", "data.description.value").extractValue(data);
      * console.log(value) // -> { "desc": "bla bla" }
      */
-    extract(data: TranslatableData): Record<string, string> {
+    extract(data: TranslatableData): Record<string, unknown> {
         return {
             [this.field]: this.extractValue(data),
         };
